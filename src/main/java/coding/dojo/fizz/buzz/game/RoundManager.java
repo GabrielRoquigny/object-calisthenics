8d71fef27514b3b8495d0f0f57cd58c6e1506882
ROQUIGNY, Gabriel <gabriel.roquigny@cgi.com>
2017-05-01 15:20:25 +0200

    Fizz-Buzz : Structure de classe pour gerer le tour
    - Ajout des méthodes `isMultiple` et `createOutput` sur la classe
      `RoundInput`.
    - Ajout de l'interface Rule qui défini des règles.

--- src/main/java/coding/dojo/fizz/buzz/game/RoundManager.java
+++ src/main/java/coding/dojo/fizz/buzz/game/RoundManager.java
@@ -11,6 +11,22 @@ import java.util.function.Consumer;
  * En fonction de l'entrée du joueur, elle donne le résultat du jeu.
  */
 public class RoundManager {
+
+    /**
+     * Construit le tour avec tous les règles à appliquer.
+     *
+     * @param rules les règles du tour.
+     */
+    public RoundManager(final Rule... rules) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Essaye chaque règle et appele le consomateur si la règle donne une donnée de sortie.
+     *
+     * @param roundInput     La donnée d'entrée du joueur.
+     * @param outputConsumer le consomateur de donnée de sortie.
+     */
     public void launch(RoundInput roundInput, Consumer<RoundOutput> outputConsumer) {
         throw new NotImplementedException();
     }
