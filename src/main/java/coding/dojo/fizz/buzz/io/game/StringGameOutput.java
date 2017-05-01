790f76cfccd3ea959c45d75181004db50c11b5f3
ROQUIGNY, Gabriel <gabriel.roquigny@cgi.com>
2017-05-01 22:13:15 +0200

    Fizz-Buzz : Implementation pour StringGameOutput

--- src/main/java/coding/dojo/fizz/buzz/io/game/StringGameOutput.java
+++ src/main/java/coding/dojo/fizz/buzz/io/game/StringGameOutput.java
@@ -2,7 +2,6 @@ package coding.dojo.fizz.buzz.io.game;
 
 import coding.dojo.fizz.buzz.io.GameOutput;
 import coding.dojo.fizz.buzz.io.RoundOutput;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.PrintStream;
 
@@ -10,13 +9,15 @@ import java.io.PrintStream;
  * Classe affichant les RoundOutput sur un {@link PrintStream}.
  */
 public class StringGameOutput implements GameOutput {
+    private final PrintStream stream;
+
     public StringGameOutput(PrintStream stream) {
         super();
-        throw new NotImplementedException();
+        this.stream = stream;
     }
 
     @Override
     public void addRoundOutput(RoundOutput roundOutput) {
-        throw new NotImplementedException();
+        roundOutput.display(stream);
     }
 }
